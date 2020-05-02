74543392673f3e99c7de363ef4bc028aedf3589f
Jeroen Janssen <jeroen.janssen@gmail.com>
2020-05-02 23:36:00 +0200

    add RtpSession and connect sending payload

--- index.ts
+++ index.ts
@@ -2,11 +2,82 @@ import * as dgram from 'dgram';
 
 import { PoaInput, PoaOutput } from 'pa-opus-audio';
 import Rtp = require('./rtp');
+import { createBrotliCompress } from 'zlib';
 
-let rtpClients = new Map<string, PoaOutput>();
+/* RtpSession parts based on https://github.com/MayamaTakeshi/rtp-session/blob/master/index.js MIT License
+ * Copyright (c) 2020 MayamaTakeshi
+ */
+class RtpSessionInfo {
+    payload_type: number = 0;
+    ssrc: number = (Math.random() * 4294967296) >>> 0; // random ssrc: TODO, should server hand these out?
+    seq_num: number = 1;
+    time_stamp: number = 0;
+}
+class RtpSession {
+
+    constructor(socket: dgram.Socket, remote: dgram.RemoteInfo) {
+
+        let version = 2;
+        let padding = 0;
+        let extension = 0;
+        let csrc_count = 0;
+        let marker = 0;
+        this._hdr = Buffer.alloc(12);
+        this._info = new RtpSessionInfo();
+        this._remote = remote;
+        this._socket = socket;
+
+        this.output = new PoaOutput();
+        this.output.initStartPlayback();
+
+        this._hdr[0] = (version << 6 | padding << 5 | extension << 4 | csrc_count)
+        this._hdr[1] = (marker << 7 | this._info.payload_type)
+        this._hdr[2] = 0   // seq_num MSB
+        this._hdr[3] = 0   // seq_num LSB
+        this._hdr[4] = 0   // timestamp MSB
+        this._hdr[5] = 0   // timestamp 
+        this._hdr[6] = 0   // timestamp
+        this._hdr[7] = 1   // timestamp LSB
+        this._hdr[8] = this._info.ssrc >>> 24
+        this._hdr[9] = this._info.ssrc >>> 16 & 0xFF
+        this._hdr[10] = this._info.ssrc >>> 8 & 0xFF
+        this._hdr[11] = this._info.ssrc & 0xFF
+    }
+
+    sendPayload(payload: Buffer) {
+        const buf = Buffer.concat([this._hdr, payload]);
+
+        buf[1] = (this._info.seq_num ? 1 : 0) << 7 | (this._info.payload_type)
+
+        var seq_num = this._info.seq_num
+        buf[2] = seq_num >>> 8
+        buf[3] = seq_num & 0xFF
+        this._info.seq_num++
+
+        var time_stamp = this._info.time_stamp
+        this._info.time_stamp += 160; // 48kHz/2.5ms? frames per payload? //payload.length
+
+        buf[4] = time_stamp >>> 24
+        buf[5] = time_stamp >>> 16 & 0xFF
+        buf[6] = time_stamp >>> 8 & 0xFF
+        buf[7] = time_stamp & 0xFF
+
+        this._socket.send(buf, 0, buf.length, this._remote.port, this._remote.address)
+    }
+
+    receivePayload(payload: Buffer) {
+        this.output.decodeAndPlay(payload);
+    }
+
+    _hdr: Buffer;
+    _info: RtpSessionInfo;
+    _remote: dgram.RemoteInfo;
+    _socket: dgram.Socket;
+    output: PoaOutput;
+}
 
 class LocalMonitor {
-    constructor() {
+    constructor(cb: Function) {
         this.contentCount = 0;
         this.emptyCount = 0;
         this.totalLength = 0;
@@ -17,6 +88,7 @@ class LocalMonitor {
 
         /** this is for recording mic and playback on own output device so you can hear what you say */
         this.localInput.setEncodedFrameAvailableCallback(this.encodedFrameAvailable);
+        this.encodedFrameAvailableCb = cb;
     };
 
     initInOutput() {
@@ -33,7 +105,7 @@ class LocalMonitor {
 
             if (this.logStats) {
                 if (this.contentCount % 100 == 0) {
-                    console.log(`contentCount: ${this.contentCount}, totalLength: ${this.totalLength}`);
+                    console.log(`local: contentCount: ${this.contentCount}, totalLength: ${this.totalLength}`);
                 }
             }
         }
@@ -42,19 +114,21 @@ class LocalMonitor {
 
             if (this.logStats) {
                 if (this.contentCount % 100 == 0) {
-                    console.log(`emptyCount: ${this.emptyCount}`);
+                    console.log(`local: emptyCount: ${this.emptyCount}`);
                 }
             }
         }
+        this.encodedFrameAvailableCb(b);
     }
 
-    contentCount : number;
-    emptyCount : number;
-    totalLength : number;
-    logStats : boolean;
+    contentCount: number;
+    emptyCount: number;
+    totalLength: number;
+    logStats: boolean;
 
     localInput: PoaInput;
     localOutput: PoaOutput;
+    encodedFrameAvailableCb: Function;
 }
 
 class RtpServer {
@@ -63,7 +137,9 @@ class RtpServer {
         this.server = dgram.createSocket('udp4');
         console.log(this.server);
 
-        this.monitor = new LocalMonitor();
+        this.clients = new Map<string, RtpSession>()
+
+        this.monitor = new LocalMonitor(this.onLocalMonitorEncodedFrameAvailable);
 
         // emits on new datagram msg
         this.server.on('message', this.onClientMessage);
@@ -78,21 +154,26 @@ class RtpServer {
         this.server.on('error', this.onError);
     }
 
-    onClientMessage(msg: Buffer, info: dgram.RemoteInfo) {
+    onLocalMonitorEncodedFrameAvailable = (frame:Buffer) => {
+        
+        this.clients.forEach(function (session:RtpSession) {
+            session.sendPayload(frame);
+        })
+    }
+
+    onClientMessage = (msg: Buffer, info: dgram.RemoteInfo) => {
         let clientKey = `${info.address}:${info.port}`;
         let payload = Rtp.payload(msg);
 
         //console.log(`'${clientKey}' send msg with length ${msg.length}`);
-        if (!rtpClients.has(clientKey)) {
+        if (!this.clients.has(clientKey)) {
             console.log(`NEW client detected at: ${clientKey}`);
-            let rtpClient = new PoaOutput();
-            let x = rtpClient.initStartPlayback();
-            console.log(`OutputInitAndStartStream: ${x}`);
-            rtpClients.set(clientKey, rtpClient);
+            let rtpClient = new RtpSession(this.server, info);
+            this.clients.set(clientKey, rtpClient);
         }
-        let currentClient = rtpClients.get(clientKey);
+        let currentClient = this.clients.get(clientKey);
         if (currentClient) {
-            let status = currentClient.decodeAndPlay(payload);
+            let status = currentClient.receivePayload(payload);
         }
 
         /*
@@ -137,6 +218,8 @@ class RtpServer {
 
     monitor: LocalMonitor;
     server: dgram.Socket;
+
+    clients: Map<string, RtpSession>;
 }
 
 console.log("Welcome at this rehearse20 experiment....");
